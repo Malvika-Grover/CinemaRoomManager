e99786298d6914a2fd8020b5e86202390cef7685
Malvika Grover <malvikagrover22@gmail.com>
2021-01-25 20:53:03 +0530

    Calculating Profit

--- src/cinema/Cinema.java
+++ src/cinema/Cinema.java
@@ -1,8 +1,10 @@
 package cinema;
 
+import java.util.Scanner;
 public class Cinema {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Cinema:");
         for (int i = 1; i < 9; i++) {
             System.out.print(" "+i);
@@ -10,8 +12,24 @@ public class Cinema {
         System.out.println();
         for (int j = 1; j < 8; j++) {
             System.out.println(j+" S S S S S S S S");
-
         }
+        int price = 0;
+        System.out.println("Enter the number of rows:");
+        int rows = sc.nextInt();
+        System.out.println("Enter the number of seats in each row:");
+        int seats = sc.nextInt();
+        int totalSeats = rows*seats;
+        if (totalSeats <= 60){
+            price = totalSeats * 10;
+            System.out.println("Total income:");
+            System.out.println("$"+price);
 
+        }else if (totalSeats > 60){
+            int frontRows = rows/2;
+            int price1 = seats * frontRows * 10;
+            int price2 =  seats * (rows - frontRows) * 8;
+            System.out.println("Total income:");
+            System.out.println("$"+(price1+price2));
+        }
     }
 }
