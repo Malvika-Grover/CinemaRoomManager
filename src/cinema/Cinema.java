d67f9863874fa3872a87081934734975187b09ad
Malvika Grover <malvikagrover22@gmail.com>
2021-01-27 15:46:08 +0530

    Marking seat chose by user and displaying it with price.

--- src/cinema/Cinema.java
+++ src/cinema/Cinema.java
@@ -5,13 +5,13 @@ public class Cinema {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Cinema:");
         System.out.println("Enter the number of rows:");
         int rows = sc.nextInt();
 
         System.out.println("Enter the number of seats in each row:");
         int seats = sc.nextInt();
 
+        System.out.println("Cinema:");
         int number = 1;
         System.out.print(" ");
 
@@ -22,7 +22,7 @@ public class Cinema {
 
         number = 1;
         System.out.println();
-        for (int j = 1; j < rows; j++) {
+        for (int j = 0; j < rows; j++) {
             System.out.print(number + " ");
             for (int k = 0; k < seats; k++) {
                 System.out.print("S" + " ");
@@ -31,35 +31,66 @@ public class Cinema {
             number++;
         }
         number = 1;
+
+        System.out.println();
         System.out.println("Enter a row number:");
         int chosenRow = sc.nextInt();
         System.out.println("Enter a seat number in that row:");
         int chosenSeat = sc.nextInt();
 
-        int price = 0;
-
-        int[][] total = new int[chosenRow][chosenSeat];
+        int price;
 
         int totalSeats = rows * seats;
         if (totalSeats < 60) {
             price = 10;
-            System.out.println("Total income: ");
+            System.out.print("Ticket price: ");
             System.out.println("$" + price);
 
         } else if (totalSeats > 60) {
             int frontRow = rows / 2;
             if (chosenRow > frontRow) {
                 price = 8;
-                System.out.println("Total income: ");
+                System.out.print("Ticket price: ");
                 System.out.println("$" + price);
 
             } else {
                 price = 10;
-                System.out.println("Total income: ");
+                System.out.print("Ticket price:  ");
                 System.out.println("$" + price);
             }
         }
+        System.out.println();
+        System.out.println("Cinema:");
+
+        int[][] total = new int[chosenRow][chosenSeat];
+
+        System.out.print(" ");
+
+        for (int i = 0; i < seats; i++) {
+            System.out.print(number + " ");
+            number++;
+        }
+
+        number = 1;
+        System.out.println();
+        for (int i = 1; i <=rows; i++) {
+            System.out.print(number + " ");
+
+            for (int j = 1; j <= seats; j++) {
+
+
+                if (i < chosenRow && i > chosenRow && j < chosenSeat && j > chosenSeat) {
+                    System.out.print("S"+" ");
+                }
+                if (i == chosenRow && j == chosenSeat) {
+                    System.out.print("B"+" ");
+                } else {
+                    System.out.print("S"+" ");
+                }
+            }
+            System.out.println();
+            number++;
+        }
+        number=1;
     }
 }
-
-
