f5688578e0e2ef53281e329a1fc32ab01a50414c
Malvika Grover <malvikagrover22@gmail.com>
2021-01-27 13:03:39 +0530

    Enhancing code, taking input from user(rows,seats)

--- src/cinema/Cinema.java
+++ src/cinema/Cinema.java
@@ -6,30 +6,73 @@ public class Cinema {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Cinema:");
-        for (int i = 1; i < 9; i++) {
-            System.out.print(" "+i);
-        }
-        System.out.println();
-        for (int j = 1; j < 8; j++) {
-            System.out.println(j+" S S S S S S S S");
-        }
-        int price = 0;
         System.out.println("Enter the number of rows:");
         int rows = sc.nextInt();
+
         System.out.println("Enter the number of seats in each row:");
         int seats = sc.nextInt();
-        int totalSeats = rows*seats;
-        if (totalSeats <= 60){
-            price = totalSeats * 10;
-            System.out.println("Total income:");
-            System.out.println("$"+price);
 
-        }else if (totalSeats > 60){
-            int frontRows = rows/2;
-            int price1 = seats * frontRows * 10;
-            int price2 =  seats * (rows - frontRows) * 8;
-            System.out.println("Total income:");
-            System.out.println("$"+(price1+price2));
+        int number = 1;
+        System.out.print(" ");
+
+        for (int i = 0; i < seats; i++) {
+            System.out.print(number + " ");
+            number++;
+        }
+
+        number = 1;
+        System.out.println();
+        for (int j = 1; j < rows; j++) {
+            System.out.print(number + " ");
+            for (int k = 0; k < seats; k++) {
+                System.out.print("S" + " ");
+            }
+            System.out.println();
+            number++;
         }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
